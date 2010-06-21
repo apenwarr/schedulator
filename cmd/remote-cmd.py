77673b12a63149bddb9c53ac654917262089e732
Avery Pennarun <apenwarr@gmail.com>
2010-06-21 13:11:29 -0400

    cmd/remote: print the current remote url if no parameters are given.

--- cmd/remote-cmd.py
+++ cmd/remote-cmd.py
@@ -9,14 +9,21 @@ bog remote <url>
 o = options.Options('bog remote', optspec)
 (opt, flags, extra) = o.parse(sys.argv[1:])
 
-if len(extra) != 1:
-    o.fatal('exactly one argument expected')
-
 repo.check_dir()
 os.environ['GIT_DIR'] = os.path.join(repo.check_dir(), '.git')
-
 oldurl = repo.remote_url()
-if oldurl:
-    log('old remote was: %s\n' % oldurl)
-    subprocess.call(['git', 'remote', 'rm', 'origin'])
-sys.exit(subprocess.call(['git', 'remote', 'add', '-f', 'origin', extra[0]]))
+
+if not extra:
+    if oldurl:
+        print oldurl
+    else:
+        log('No remote URL defined.\n')
+elif len(extra) != 1:
+    o.fatal('exactly one argument expected')
+else:
+    # exactly one argument
+    if oldurl:
+        log('old remote was: %s\n' % oldurl)
+        subprocess.call(['git', 'remote', 'rm', 'origin'])
+    sys.exit(subprocess.call(['git', 'remote', 'add', '-f',
+                              'origin', extra[0]]))
