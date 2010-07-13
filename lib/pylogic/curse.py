2abfd515afc7a9bf033253615bae8e2449823d14
Avery Pennarun <apenwarr@gmail.com>
2010-07-13 03:38:50 -0400

    curse.py: support adding/removing child views from a view.

--- lib/pylogic/curse.py
+++ lib/pylogic/curse.py
@@ -94,23 +94,90 @@ class Pos(object):
 Size = Pos
 
 
+class Area(object):
+    __slots__ = ['x1', 'y1', 'x2', 'y2']
+
+    def __init__(self, pos, size):
+        (self.x1, self.y1) = (pos.x, pos.y)
+        size = Pos(max(size.x, 0), max(size.y, 0))
+        (self.x2, self.y2) = (pos.x + size.x - 1, pos.y + size.y - 1)
+
+    def __repr__(self):
+        return 'Area(%d,%d,%d,%d)' % (self.x1, self.y1, self.x2, self.y2)
+
+    def size(self):
+        return Size(self.x2 - self.x1 + 1, self.y2 - self.y1 + 1)
+
+    def intersect(self, area):
+        topleft = Pos(max(self.x1, area.x1), max(self.y1, area.y1))
+        botright = Pos(min(self.x2, area.x2), min(self.y2, area.y2))
+        return Area(topleft, botright - topleft + Pos(1,1))
+
+
+class _Child(object):
+    __slots__ = ['child', 'anchor', 'pos', 'size']
+
+    def __init__(self, child, anchor, pos, size):
+        (self.child, self.anchor, self.pos, self.size) \
+            = (child,anchor,pos,size)
+
+    def __repr__(self):
+        return 'C(%r,%r,%r,%r)' % (self.child, self.anchor, self.pos, self.size)
+
+
 class View:
     def __init__(self):
         self.parent = None
+        self.children = []
         self.w = self.pos = self.size = None
+        self._needs_layout = True
+
+    def add(self, child, anchor=None, pos=None, size=None):
+        if anchor:
+            assert(not pos)
+        if not anchor:
+            if not pos:
+                pos = child.pos
+            if not size:
+                size = child.size
+            assert(pos)
+            assert(size)
+        child._setparent(self)
+        self.children.append(_Child(child, anchor, pos, size))
+        if size:
+            child._setsize(size)
+            child._setpos(Pos(0,0))
+        self._needs_layout = True
+        return child
+
+    def remove(self, child):
+        for i,c in enumerate(self.children):
+            if c.child == child:
+                del self.children[i]
+                c.child._setparent(None)
+                break
 
     def _setparent(self, parent):
-        if parent:
-            self.parent = weakref.ref(parent)
-        else:
-            self.parent = None
+        p = self.parent and self.parent() or None
+        if p != parent:
+            if parent:
+                self.parent = weakref.ref(parent)
+            else:
+                self.parent = None
+            self._needs_layout = True
 
     def _setpos(self, pos):
-        self.pos = pos
+        if self.pos != pos:
+            self.pos = pos
 
     def _setsize(self, size):
-        self.size = size
-        self.w = curses.newpad(size.y, size.x)
+        if self.size != size:
+            self.size = size
+            self.w = curses.newpad(size.y, size.x)
+            self._needs_layout = True
+
+    def area(self):
+        return Area(self.pos, self.size)
 
     def gpos(self):
         if self.parent:
@@ -118,21 +185,42 @@ class View:
         else:
             return self.pos
 
-    def _render(self):
-        ipos = Pos(-min(self.pos.x, 0), -min(self.pos.y, 0))
-        gpos = self.gpos() + ipos
-        size = self.size - ipos
+    def garea(self):
         if self.parent:
-            psize = self.parent().size
+            return Area(self.gpos(), self.size)
         else:
-            psize = size
-        if self.pos.x + size.x > psize.x:
-            size.x = psize.x - self.pos.x
-        if self.pos.y + size.y > self.size.y:
-            size.y = psize.y - self.pos.y
-        self.w.noutrefresh(ipos.y, ipos.x,
-                           gpos.y, gpos.x, gpos.y+size.y-1, gpos.x+size.x-1)
+            return Area(self.pos, self.size)
 
+    def _do_layout(self):
+        for c in self.children:
+            c.child._setsize(c.size)
+            c.child._setpos(c.pos)
+        self._needs_layout = False
+
+    def _render_me(self):
+        area = self.garea()
+        parea = self.parent().garea()
+        remain = area.intersect(parea)
+        if remain.size().x and remain.size().y:
+            offset = Pos(remain.x1 - area.x1, remain.y1 - area.y1)
+            try:
+                self.w.noutrefresh(offset.y, offset.x,
+                                   remain.y1, remain.x1, remain.y2, remain.x2)
+            except:
+                raise Exception('refresh error: %r' %
+                                ((area, parea, remain, offset),))
+
+    def _render(self):
+        if self._needs_layout:
+            self._do_layout()
+
+        self._render_me()
+
+        # do this *after* our own display, because the children should be
+        # drawn on top.
+        for c in self.children:
+            c.child._render()
+        
     def fill(self, c, at):
         self.w.bkgd(c, at)
         self.w.border()
@@ -165,13 +253,16 @@ class Screen(View):
     def __exit__(self, type,value,traceback):
         curses.endwin()
 
-    def __iter__(self):
-        return self
+    def _render_me(self):
+        self.w.redrawwin()
+        self.w.noutrefresh()
+        pass
 
     def setcursor(self, pos):
         self.cursorpos = pos
 
     def refresh(self):
+        self._render()
         if self.cursorpos:
             curses.setsyx(max(0, self.cursorpos.y), max(0, self.cursorpos.x))
             try:
@@ -198,37 +289,51 @@ class Screen(View):
             os.read(sys.stdin.fileno(), 4096)  # *up to* 4096 bytes
 
 
-with Screen() as s:
-    w = curses.newpad(s.size.y, s.size.x)
-    w.bkgd('.', color(BLACK,xBLUE))
-    w.addstr("\n\n   wonko the sane  \n\nbane", color(RED, xBLACK))
-    w.addstr("\n\n   wonko the sane  \n\nbane", color(xRED, BLUE, UNDERLINE))
-    w.border()
+if 1:
+    with Screen() as s:
+        w = s.add(View(), size=s.size, pos=Pos(0,0))
+        w.fill('.', color(BLACK,xBLUE))
+        w.w.addstr("\n\n   wonko the sane  \n\nbane",
+                   color(RED, xBLACK))
+        w.w.addstr("\n\n   wonko the sane  \n\nbane",
+                   color(xRED, BLUE, UNDERLINE))
 
-    p = curses.newpad(10,10)
-    p.bkgd('!', color(RED, YELLOW))
-    w.noutrefresh(0,0, 0,0,s.size.y-1,s.size.x-1)
-    p.noutrefresh(0,0, 10,20,10+10-1,20+10-1)
+        p = s.add(View(), size=Size(10,10), pos=Pos(20,10))
+        p.fill('!', color(RED, YELLOW))
 
-    v = View()
-    v._setparent(s)
-    v._setpos(Pos(-5,6))
-    v._setsize(Size(30,600))
-    v.fill('Q', color(xYELLOW,BLACK))
-    v._render()
+        v = s.add(View(), pos=Pos(-5,6), size=Size(30,600))
+        v.fill('Q', color(xYELLOW,BLACK))
 
-    s.runonce()
-    
-    v.setcursor(Pos(1,1))
-    s.runonce()
-    
-    v.setcursor(None)
-    s.runonce()
-    
-    n = curses.COLOR_PAIRS
-    nn = curses.COLORS
-    ccc = curses.can_change_color()
+        v4 = s.add(View(), pos=Pos(10,8), size=Size(30,5))
+        v4.fill('R', color(xBLUE,BLACK))
 
-print n
-print nn
-print ccc
+        s.runonce()
+
+        v.setcursor(Pos(1,1))
+        s.runonce()
+
+        v.setcursor(None)
+        s.runonce()
+
+        s.remove(v)
+        s.runonce()
+
+        s.add(v)
+        s.runonce()
+
+        for i in range(s.size.y):
+            s.remove(v)
+            s.add(v, pos=Pos(i*10,i))
+            s.runonce(0.05)
+
+            n = curses.COLOR_PAIRS
+            nn = curses.COLORS
+            ccc = curses.can_change_color()
+
+            print n
+            print nn
+            print ccc
+
+if 1:
+    with Screen() as s:
+        pass
